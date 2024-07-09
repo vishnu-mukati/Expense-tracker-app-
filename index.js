b79ef9ae3d6e59c46420672dcf02db4a9eeb7621
vishnu mukati <mukativishnu1232@gmail.com>
2024-07-09 15:35:13 +0530

    intigrating bootstrap for the 3 hours of advance javascript project

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function handformsubmit(event) {
+function handleformsubmit(event) {
     event.preventDefault();
 
     let expenseamount = event.target.expenseamount.value;
@@ -6,36 +6,53 @@ function handformsubmit(event) {
     let category = event.target.category.value;
 
     let obj = {
-        "expenseamount": expenseamount,
-        "description": description,
-        "category": category,
+        expenseamount: expenseamount,
+        description: description,
+        category: category,
     }
 
     let obj_serialize = JSON.stringify(obj)
 
     localStorage.setItem(obj.expenseamount, obj_serialize);
-    shouwuseronscreen(obj);
+    showUserOnScreen(obj);
 
     event.target.expenseamount.value = '';
     event.target.description.value = '';
     event.target.category.value = '';
 }
 
-function shouwuseronscreen(obj) {
+function showUserOnScreen(obj) {
 
-    let ul = document.getElementById('userlist');
+    let tablebody = document.querySelector('#tablebody');
 
-    let li = document.createElement('li');
-    li.textContent = `${expenseamount.value}-${description.value}-${category.value} `;
+
+    let row = document.createElement('tr');
+
+    let amountcell = document.createElement('td');
+    amountcell.textContent = obj.expenseamount;
+    row.appendChild(amountcell);
+
+    let descriptioncell = document.createElement('td');
+    descriptioncell.textContent = obj.description;
+    row.appendChild(descriptioncell);
+
+    let categorycell = document.createElement('td');
+    categorycell.textContent = obj.category;
+    row.appendChild(categorycell);
+
+
+    let actioncell = document.createElement('td');
+    row.appendChild(actioncell);
 
     let EditExpense = document.createElement('button');
     EditExpense.textContent = 'Edit Expense';
-    li.appendChild(EditExpense)
+    EditExpense.className = "btn btn-outline-info border-2 btn-xxl"
+    actioncell.appendChild(EditExpense)
     EditExpense.onclick = () => {
         let storedobj = JSON.parse(localStorage.getItem(obj.expenseamount));
         localStorage.removeItem(obj.expenseamount);
-        ul.removeChild(li);
-        let inputitem = document.querySelectorAll('#expenseamount,#description,#category')
+        tablebody.removeChild(row);
+        let inputitem = document.querySelectorAll('#expenseamount, #description, #category')
         inputitem[0].value = storedobj.expenseamount;
         inputitem[1].value = storedobj.description;
         inputitem[2].value = storedobj.category;
@@ -43,16 +60,16 @@ function shouwuseronscreen(obj) {
 
     let deleteExpense = document.createElement('button');
     let deleteExpensetext = document.createTextNode('DeleteExpense');
-    deleteExpense.className = 'delete-btn';
+    deleteExpense.className = 'btn btn-outline-danger border-2 btn-xxl';
     deleteExpense.type = 'button'
 
     deleteExpense.appendChild(deleteExpensetext);
+    actioncell.appendChild(deleteExpense)
     deleteExpense.addEventListener('click', function (event) {
-        if (event.target.classList.contains('delete-btn')) {
+        if (event.target.classList.contains('btn')) {
             localStorage.removeItem(obj.expenseamount)
-            ul.removeChild(li)
+            tablebody.removeChild(row)
         }
     })
-    li.appendChild(deleteExpense)
-    ul.appendChild(li)
+    tablebody.appendChild(row)
 }
